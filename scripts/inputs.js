7657d36e3d9e981394cf39abf1ba6f97ac49bc01
Johan-Liebert1 <pragyanpoudyal41999@gmail.com>
2020-08-19 15:15:19 +0530

    Remove annoying alerts

--- scripts/inputs.js
+++ scripts/inputs.js
@@ -2,28 +2,19 @@ function keypressed (event, row, col, current_cell) {
     event.preventDefault()
     keys = ['1', '2', '3', '4', '5','6','7', '8', '9', 'Backspace']
 
-    if (!
-        ((49 <= event.keyCode && event.keyCode <= 57) 
-        || 
-        (97 <= event.keyCode && event.keyCode <= 105)
-        || (event.keyCode === 8)
-    )) {
-        alert("Only supported keys are Numbers and Backspace.                          \
-        If you're seeing this even though you've pressed a number key, then you might have your numlock off")
+    number = event.key
 
-    }
+    if (keys.includes(event.key)) {
+        
+        if (event.keyCode !== 8) {
 
-    else {
-        number = event.key
-
-        if (keys.includes(event.key)){
-            if (event.keyCode !== 8){
             event.target.value = number
 
             if (!check_number_validity(current_board_unsolved, Number(number), row, col)){
                 current_board_unsolved[row][col] = Number(number)
                 current_cell.setAttribute("style", "color: red")
             }
+
             else{
                 current_board_unsolved[row][col] = Number(number)
                 current_cell.setAttribute("style", "color: green")
@@ -33,9 +24,11 @@ function keypressed (event, row, col, current_cell) {
         else {
             event.target.value = ''
         }
-        
-        }
+    
     }
+    
+
+
     if (!find_unfilled(current_board_unsolved)){
         win_dialog = document.getElementById('you-win')
         win_dialog.classList.remove('gameover-dialog')
